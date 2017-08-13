82169f5bfc83dd515d995b4b64f0b2d1909d8387
Zoltan Gilian <zoltan.gilian@gmail.com>
2017-08-13 17:52:03 +0200

    update CI files

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 from conan.packager import ConanMultiPackager
 
+
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(username="zogi", channel="stable")
     builder.add_common_builds(shared_option_name="OpenEXR:shared")
     builder.run()
