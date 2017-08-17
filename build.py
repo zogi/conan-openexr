879caf3f1028882d5c51a1ae8a75d87f91d6e569
Zoltan Gilian <zoltan.gilian@gmail.com>
2017-08-17 06:50:25 +0200

    Fix conan usernames to match with upstream

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="zogi", channel="stable", args="--build=missing")
+    builder = ConanMultiPackager(username="Mikayex", channel="stable", args="--build=missing")
     builder.add_common_builds(shared_option_name="OpenEXR:shared")
     builder.run()
